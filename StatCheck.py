4165ceaa3328459b5f7483a378a3d515ea4c642f
Andrii Naumov <59359275+andr-nau@users.noreply.github.com>
2020-03-11 08:56:44 +0100

    Add files via upload

---
+++ StatCheck.py
@@ -0,0 +1,32 @@
+#StatCheck
+#Just test Python code to play with libraries
+
+import random
+import statistics
+import numpy
+import matplotlib
+
+
+b=[]
+
+#Get 10'000 random numbers 0-10 
+f = open("myfile.txt", "w")
+for i in range(0,10000):
+    a=random.randint(1,10)
+    b.append(int(a))
+    f.write(str(a)+" ")
+
+#Check stats
+mn = statistics.mean(b)
+mnn = numpy.mean(b)
+med = statistics.median(b)
+medn = numpy.median(b)
+qtn = numpy.quantile(b,0.5)
+
+text = "Mean:\n"+"stat:"+str(mn)+"\nnumpy:"+str(mnn)+"\nMediane:\n"+"stat:"+str(med)+"\nnumpy:"+str(medn)+"\nQuantile:"+"\nnumpy:"+str(qtn)
+print(text)
+f.write(text)
+
+matplotlib.pyplot.hist(b,bins=10,rwidth=0.6)
+
+f.close()
